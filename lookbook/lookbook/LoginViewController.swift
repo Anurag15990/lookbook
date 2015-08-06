ba79bc4849dfd9110be8fbb593f3b83329f0cade
Anurag <anurag.agnihotri@ensuantinc.com>
2015-08-06 09:40:40 +0530

    Login View

---
+++ lookbook/lookbook/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  lookbook
+//
+//  Created by Anurag Agnihotri on 05/08/15.
+//  Copyright (c) 2015 lookbook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var tabView : UIView!
+    @IBOutlet weak var signInView : UIView!
+    @IBOutlet weak var signInEmailTextField : UITextField!
+    @IBOutlet weak var signInPasswordTextField : UITextField!
+    @IBOutlet weak var forgotPasswordButton : UIButton!
+    @IBOutlet weak var signInButton : UIButton!
+    @IBOutlet weak var signInFacebookView : UIView!
+    @IBOutlet weak var signInFacebookIconLabel : UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
